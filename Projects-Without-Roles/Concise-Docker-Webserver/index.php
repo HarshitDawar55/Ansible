db17050d5314d45cd6a7890d65d6c9faed607bcf
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-11-26 18:54:57 +0530

    Working Code

--- Projects-Without-Roles/Concise-Docker-Webserver/index.php
+++ Projects-Without-Roles/Concise-Docker-Webserver/index.php
@@ -1,19 +1,4 @@
 <body bgcolor = "aqua">
+<h1>Welcome to Webserver by Mr. HarshitDawar!</h1>
 
-<pre>
 
-<?php
-
-print "Welcome to Webserver by <b>Mr. HarshitDawar! </b> \n\n\n";
-
-echo '<b> <a href="https://harshitdawar55.github.io"> Link to my PortFolio! </a> </b>';
-
-print "\n\n\n";
-
-print `ifconfig`;
-
-print `free -m`;
-
-?>
-
-</pre>
